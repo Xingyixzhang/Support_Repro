31118decfc3a642c30a86606e94cc20d45e1fef0
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2021-12-26 12:07:13 -0800

    Update Program.cs

--- EventHubTrigger-WeatherApp/WeatherApp/Program.cs
+++ EventHubTrigger-WeatherApp/WeatherApp/Program.cs
@@ -8,13 +8,13 @@ namespace WeatherApp
 {
     public class Program
     {
-        public static string ConnectionString = "Endpoint=sb://xingyieh.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9f/lbILhdlpjGwhg6VS6MveM11j1wn6k1PWlMO/MAmk=;EntityPath=eh001tdptriggertest";
+        public static string ConnectionString = "YourEHConnectionStringWithEntityName";
 
         static void Main(string[] args)
         {
             SenderHelper helper = new SenderHelper(ConnectionString);
             
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < 5; i++) // execution iterations
             {
                 helper.DataSender();
                 Thread.Sleep(1500); // <=200 ms is not working. 1500> x >=300 executed but not exactly in steps. Doesn't matter how many times the loop ran.
